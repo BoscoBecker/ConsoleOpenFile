bab71c916d70fb90b237641f143db62540404dbc
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-15 09:31:17 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleOpenFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleOpenFile{
+    class Program {
+        public static void Main() {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(@"D:\Projetos\C#\ConsoleOpenFile\ConsoleOpenFile\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                while (!sr.EndOfStream) {
+                        Console.WriteLine(sr.ReadLine());
+                    }                      
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (fs != null)
+                {
+                    fs.Close(); 
+                    fs.Dispose();
+                }
+            }        
+        }
+    }
+}
+
+
